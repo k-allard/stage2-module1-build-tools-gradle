0b81c3027291ec71ffae3632466ef234abc81e9f
Anastasiia Smirnova <smirn.anastasiia@gmail.com>
2023-04-27 18:10:34 +0400

    Handle non-valid numbers starting with 0

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
+        if (str != null && str.length() > 0 && str.charAt(0) == '0')
+            return false;
         return NumberUtils.toInt(str) > 0
                 || NumberUtils.toDouble(str) > 0;
     }
