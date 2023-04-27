06c70af4bfbaaf07f66cba2780c2d06610252e5a
Viacheslav Smirnov <owasp@yandex.ru>
2023-04-27 16:32:03 +0400

    It compiles

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -2,8 +2,14 @@ package com.epam.demo;
 
 import java.util.List;
 
+import static com.epam.utils.StringUtils.isPositiveNumber;
+
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        //magic happens here
+        for (String arg : args) {
+            if (!isPositiveNumber(arg))
+                return false;
+        }
+        return true;
     }
 }
